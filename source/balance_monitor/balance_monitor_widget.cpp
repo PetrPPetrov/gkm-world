bc0c8e8f2d3dbe03a5591092dab93e46b60243b8
Petr Petrov <petr@gkmsoft.ru>
2019-12-11 20:56:30 +0300

    Implemented basic grid moving & zooming

--- source/balance_monitor/balance_monitor_widget.cpp
+++ source/balance_monitor/balance_monitor_widget.cpp
@@ -15,42 +15,51 @@ BalanceMonitorWidget::BalanceMonitorWidget(QWidget *parent) : QWidget(parent)
 void BalanceMonitorWidget::paintEvent(QPaintEvent* event)
 {
     QPainter painter(this);
-    QRect viewport = painter.viewport();
 
-    QMatrix set_center;
-    set_center.translate(viewport.center().x(), viewport.center().y());
-    QMatrix mirror_y;
-    mirror_y.scale(1.0, -1.0);
-    QTransform result_transformation(mirror_y * set_center);
-    painter.setTransform(result_transformation);
+    cached_zoom = zoom();
+    const QRect viewport = painter.viewport();
+    screen_center_x = viewport.center().x();
+    screen_center_y = viewport.center().y();
 
-    std::int32_t width = viewport.width() / actualCellSize();
-    std::int32_t height = viewport.height() / actualCellSize();
-
-    QPen thin_pen;
-    thin_pen.setColor(QColor(0, 48, 0));
-    thin_pen.setStyle(Qt::DashDotLine);
-    painter.setPen(thin_pen);
-
-    const std::int32_t min_cell_x_to_draw = -width / 2 - 1;
-    const std::int32_t max_cell_x_to_draw = width / 2 + 1;
-    const std::int32_t min_cell_y_to_draw = -height / 2 - 1;
-    const std::int32_t max_cell_y_to_draw = height / 2 + 1;
-
-    int min_x_to_draw = static_cast<int>(actualCellSize() * min_cell_x_to_draw);
-    int max_x_to_draw = static_cast<int>(actualCellSize() * max_cell_x_to_draw);
-    int min_y_to_draw = static_cast<int>(actualCellSize() * min_cell_y_to_draw);
-    int max_y_to_draw = static_cast<int>(actualCellSize() * max_cell_y_to_draw);
-
-    for (std::int32_t x = min_cell_x_to_draw; x < max_cell_x_to_draw; ++x)
+    const double world_to_draw_width = viewport.width() / cached_zoom;
+    const double world_to_draw_height = viewport.height() / cached_zoom;
+    const double actual_cell_size = CELL_SIZE * cached_zoom;
+    bool draw_cell_lines = true;
+    if (actual_cell_size < 8.0)
     {
-        int cur_x = static_cast<int>(actualCellSize() * x);
-        painter.drawLine(cur_x, min_y_to_draw, cur_x, max_y_to_draw);
+        draw_cell_lines = false;
     }
-    for (std::int32_t y = min_cell_y_to_draw; y < max_cell_y_to_draw; ++y)
+
+    if (draw_cell_lines)
     {
-        int cur_y = static_cast<int>(actualCellSize() * y);
-        painter.drawLine(min_x_to_draw, cur_y, max_x_to_draw, cur_y);
+        const std::int32_t cells_to_draw_width = static_cast<std::int32_t>(world_to_draw_width / CELL_SIZE) + 1;
+        const std::int32_t cells_to_draw_height = static_cast<std::int32_t>(world_to_draw_height / CELL_SIZE) + 1;
+        const std::int32_t cur_cell_x = static_cast<std::int32_t>(view_point_x / CELL_SIZE);
+        const std::int32_t cur_cell_y = static_cast<std::int32_t>(view_point_y / CELL_SIZE);
+        const std::int32_t start_cell_x = cur_cell_x - cells_to_draw_width / 2 - 1;
+        const std::int32_t start_cell_y = cur_cell_y - cells_to_draw_height / 2 - 1;
+        const std::int32_t end_cell_x = cur_cell_x + cells_to_draw_width / 2 + 1;
+        const std::int32_t end_cell_y = cur_cell_y + cells_to_draw_height / 2 + 1;
+
+        QPen thin_pen;
+        thin_pen.setColor(QColor(0, 148, 0));
+        thin_pen.setStyle(Qt::DotLine);
+        painter.setPen(thin_pen);
+
+        for (std::int32_t x = start_cell_x; x <= end_cell_x; ++x)
+        {
+            painter.drawLine(
+                to_screen_x(x * CELL_SIZE), to_screen_y(start_cell_y * CELL_SIZE),
+                to_screen_x(x * CELL_SIZE), to_screen_y((end_cell_y + 1) * CELL_SIZE)
+            );
+        }
+        for (std::int32_t y = start_cell_y; y <= end_cell_y; ++y)
+        {
+            painter.drawLine(
+                to_screen_x(start_cell_x * CELL_SIZE), to_screen_y(y * CELL_SIZE),
+                to_screen_x((end_cell_x + 1) * CELL_SIZE), to_screen_y(y * CELL_SIZE)
+            );
+        }
     }
 }
 
@@ -68,8 +77,50 @@ void BalanceMonitorWidget::wheelEvent(QWheelEvent* event)
     update();
 }
 
-double BalanceMonitorWidget::actualCellSize() const
+void BalanceMonitorWidget::mouseMoveEvent(QMouseEvent* event)
+{
+    if (left_mouse_pressed)
+    {
+        const QPointF current_point = event->localPos();
+        const QPointF delta = previous_point - current_point;
+        const double actual_zoom = zoom();
+        view_point_x = previous_view_point_x + delta.x() / actual_zoom;
+        view_point_y = previous_view_point_y - delta.y() / actual_zoom;
+        update();
+    }
+}
+
+void BalanceMonitorWidget::mousePressEvent(QMouseEvent* event)
+{
+    if (event->buttons() & Qt::LeftButton)
+    {
+        left_mouse_pressed = true;
+        previous_point = event->localPos();
+        previous_view_point_x = view_point_x;
+        previous_view_point_y = view_point_y;
+    }
+}
+
+void BalanceMonitorWidget::mouseReleaseEvent(QMouseEvent* event)
+{
+    left_mouse_pressed = false;
+    previous_point = event->localPos();
+    previous_view_point_x = view_point_x;
+    previous_view_point_y = view_point_y;
+}
+
+int BalanceMonitorWidget::to_screen_x(double x_) const
+{
+    return static_cast<int>((x_ - view_point_x) * cached_zoom + screen_center_x);
+}
+
+int BalanceMonitorWidget::to_screen_y(double y_) const
+{
+    return static_cast<int>((y_ - view_point_y) * -cached_zoom + screen_center_y);
+}
+
+double BalanceMonitorWidget::zoom() const
 {
     const double scale = std::pow(ZOOM_BASE, INITIAL_ZOOM_STEP);
-    return CELL_SIZE * std::pow(ZOOM_BASE, zoom_step) / scale;
+    return std::pow(ZOOM_BASE, zoom_step) / scale;
 }
--- source/balance_monitor/balance_monitor_widget.h
+++ source/balance_monitor/balance_monitor_widget.h
@@ -18,15 +18,29 @@ public:
 protected:
     void paintEvent(QPaintEvent* event) override;
     void wheelEvent(QWheelEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
 
 private:
-    double actualCellSize() const;
+    double view_point_x = 0.0;
+    double view_point_y = 0.0;
+    double previous_view_point_x = 0.0;
+    double previous_view_point_y = 0.0;
 
-private:
-    CellIndex view_point;
+    QPointF previous_point;
+    bool left_mouse_pressed = false;
+
+    double screen_center_x = 0.0;
+    double screen_center_y = 0.0;
+
+    int to_screen_x(double x_) const; // We do not use worldTransformation property of QPainter
+    int to_screen_y(double y_) const;
+    double zoom() const;
+    double cached_zoom = 1.0;
     const double ZOOM_BASE = 1.1;
     const std::int32_t INITIAL_ZOOM_STEP = 20;
-    const std::int32_t MIN_ZOOM_STEP = 13;
+    const std::int32_t MIN_ZOOM_STEP = 5;
     const std::int32_t MAX_ZOOM_STEP = 60;
     std::int32_t zoom_step = INITIAL_ZOOM_STEP;
 };
